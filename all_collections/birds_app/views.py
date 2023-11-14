687617f2ec9264f1b5ea091d2ae4286d87c74363
katiepestotnik <katie.pestotnik@gmail.com>
2023-11-14 14:45:35 -0700

    add index view and bird details to all birds

--- all_collections/birds_app/views.py
+++ all_collections/birds_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Bird
 # Create your views here.
-def home(request):
-  return render(request, 'home.html')
+# def home(request):
+#   return render(request, 'home.html')
+
+class BirdList(ListView):
+  #default context bird_list
+  model = Bird
